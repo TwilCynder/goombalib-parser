ca081a5fea0b36972d4a208a0704fc59581cd246
TwilCynder <twilcynder@gmail.com>
2023-09-20 10:50:18 +0200

    changed entry point

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 import * as m from "./parseArguments.js";
 
-////Comment to trigger github workflow
+/////Comment to trigger github workflow
 console.log("This is Goomba Lib. Hello World !");
 console.log(m.Parser);
