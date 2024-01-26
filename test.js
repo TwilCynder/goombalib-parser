83436c9e12d253883673b51405e61984e165ac3a
TwilCynder <twilcynder@gmail.com>
2024-01-26 01:00:42 +0100

    fixed singleoptionparser

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ import * as m from "./main.js";
 ///////Comment to trigger github workflow
 console.log("This is Goomba Lib. Hello World !");
 
-let [option, arg] = m.parseArguments(process.argv.slice(2), new m.SingleOptionParser("-f") , new m.SingleArgumentParser(true));
+let [option, arg] = m.parseArguments(process.argv.slice(2), new m.SingleOptionParser("-f") , new m.SingleArgumentParser(false));
 console.log(m.usageMessage("one-argument a-second-argument"));
 
 console.log(option, arg);
